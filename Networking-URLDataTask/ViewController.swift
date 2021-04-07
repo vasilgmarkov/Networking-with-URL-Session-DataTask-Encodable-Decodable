6f578c045e6216b1d77fdf44b9c43eb5c2061ca1
Vasil Markov <vasilgmarkov@gmail.com>
2021-04-07 15:00:11 +0200

    Networking with URL Session DataTask (GET & POST) methods

--- Networking-URLDataTask/ViewController.swift
+++ Networking-URLDataTask/ViewController.swift
@@ -11,9 +11,56 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sendPost()
+        
+    }
+    
+    func sendPost() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            fatalError("Could not be a url")
+        }
+        
+        let params = [
+            "title": "I am the king!",
+            "body": "The king is back. Get over it faaaam"
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+        
+        let session = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+
+            if let error = error {
+                print("There was an error \(error.localizedDescription)")
+            } else {
+                let jsonRes = try? JSONSerialization.jsonObject(with: data!, options: [])
+                
+                print("The response: \(jsonRes)")
+            }
+            
+        }.resume()
     }
 
-
+    func getPosts() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            fatalError("Could not be a url")
+        }
+        
+        let session = URLSession.shared.dataTask(with: url) { data, responce, error in
+            
+            if let error = error {
+                print("There was an error \(error.localizedDescription)")
+            } else {
+                let jsonRes = try? JSONSerialization.jsonObject(with: data!, options: [])
+                
+                print("The response: \(jsonRes)")
+            }
+            
+        }.resume()
+    }
 }
 
